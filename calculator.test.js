9aac6725e23eebe5ec205986020a95db1f1dc211
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2024-08-05 18:14:00 -0400

    Added more calculator tests

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,15 @@ const calculator = require('./calculator.js');
 test('add two numbers', () => {
   expect(calculator.add(2, 2)).toBe(4);
 });
+
+test('subtract two numbers', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test('multiply two numbers', () => {
+  expect(calculator.multiply(3, 2)).toBe(6);
+});
+
+test('divide two numbers', () => {
+  expect(calculator.divide(2, 2)).toBe(1);
+});
